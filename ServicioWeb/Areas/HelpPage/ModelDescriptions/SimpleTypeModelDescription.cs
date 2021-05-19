c4efb6c5ee21b674f09b1c475ae39c9f5c2a44a0
Markos Ayala <elmarkos23@gmail.com>
2021-05-18 21:16:23 -0500

    ok

---
+++ ServicioWeb/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ServicioWeb.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ ServicioWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ServicioWeb.WebApiApplication" Language="C#" %>
