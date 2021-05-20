03c571f2800a2bc12eb2b28963a10d6954391391
Markos Ayala <elmarkos23@gmail.com>
2021-05-20 08:24:07 -0500

    ok
    ok

--- Documentacion/SCRIPT REPORTE.sql
+++ Documentacion/SCRIPT REPORTE.sql
@@ -4,3 +4,5 @@ INNER JOIN AsistenciaDetalle AS AD ON A.id=AD.idAsistencia
 INNER JOIN Usuario AS U ON U.id=A.idUsuario
 INNER JOIN Departamento AS D on d.id=u.idDepartamento
 ORDER BY A.fecha,ad.tipo
+
+
